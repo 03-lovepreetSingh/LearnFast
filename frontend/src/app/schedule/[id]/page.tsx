c98a32564434def0c121c83fbd4e09c5c094dd7c
Aryan Bains <aryanbains6@gmail.com>
2025-02-23 23:44:05 +0530

    Bug Fixes

--- frontend/src/app/schedule/[id]/page.tsx
+++ frontend/src/app/schedule/[id]/page.tsx
@@ -1,5 +1,5 @@
 // src/app/schedule/[id]/page.tsx
-import ViewScheduleClient from "./ViewScheduleClient";
+import ViewScheduleClient from './ViewScheduleClient';
 
 interface PageProps {
   params: {
@@ -12,12 +12,3 @@ const ViewSchedulePage = async ({ params }: PageProps) => {
 };
 
 export default ViewSchedulePage;
-export default async function ViewSchedulePage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const { id } = await params; // Ensure it's awaited
-
-  return <ViewScheduleClient scheduleId={id} />;
-}
